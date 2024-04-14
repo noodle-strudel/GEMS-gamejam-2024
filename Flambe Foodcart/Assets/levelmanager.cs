56ff53f861bb17665a3ff3dd62681138370ea0e0
Hannah <theimperialchihuahua@gmail.com>
2024-04-14 16:46:58 -0700

    new scenes

---
+++ Flambe Foodcart/Assets/levelmanager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelmanager : MonoBehaviour
+{
+
+    public string sceneName;
+    public string credit;
+    public string title;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void changeScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+    public void creditScene()
+    {
+        SceneManager.LoadScene(credit);
+    }
+
+
+    public void titleScreen()
+    {
+        SceneManager.LoadScene(title);
+    }
+}
